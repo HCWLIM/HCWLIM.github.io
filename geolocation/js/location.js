f22f4db36828bb1f75e097f359dbdfaa1e9d9517
Heng CW <weiwei5501@gmail.com>
2021-10-01 21:58:38 +0800

    0958

---
+++ geolocation/js/location.js
@@ -0,0 +1,14 @@
+let ellocate = document.getElementById("btnlocate")
+ellocate.addEventListener("click",function(){
+
+if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(function(position){
+        let elLat = document.getElementById("lat")
+        let elLong = document.getElementById("long")
+        elLat.innerHTML = "Latitude: " + position.coords.latitude
+        elLong.innerHTML = "Longtitude: " + position.coords.longitude
+    })
+}else{
+    alert("Geolocation is not supported")
+}
+})
