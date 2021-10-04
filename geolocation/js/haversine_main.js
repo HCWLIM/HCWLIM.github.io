9a04f2c7151bc0ebcb24a11bfc4647cf5c7eab4e
Heng CW <weiwei5501@gmail.com>
2021-10-04 20:26:26 +0800

    2026

--- geolocation/js/haversine_main.js
+++ geolocation/js/haversine_main.js
@@ -1,7 +1,7 @@
-function calculateDistances(lat,long){
-    let depulze = haversine(lat, long, 2.922561,101.650965)
-    let pyramid = haversine(lat, long, 3.073065,101.607787)
-    let klcc = haversine(lat, long, 3.158761,101.714524)
+function calculateDistances(lat,lon){
+    let depulze = haversine(lat, lon, 2.922561,101.650965)
+    let pyramid = haversine(lat, lon, 3.073065,101.607787)
+    let klcc = haversine(lat, lon, 3.158761,101.714524)
 
     return (depulze, pyramid, klcc)
 }
@@ -11,18 +11,18 @@ elLocate.addEventListener("click", function(){
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(function(position){
             let elLat = document.getElementById("lat")
-            let elLong = document.getElementById("long")
+            let elLon = document.getElementById("long")
             let elduplze = document.getElementById("depulze")
             let elKlcc = document.getElementById("klcc")
             let elSunway = document.getElementById("sunway")
 
             let userLat = position.coords.Latitude
-            let userLong = position.coords.Longtitude
+            let userLon = position.coords.Longtitude
             
-            let distances = calculateDistances(userLat, userLong)
+            let distances = calculateDistances(userLat, userLon)
 
             elLat.innerHTML = "Your Latitude: " + userLat
-            elLong.innerHTML = "Your longtitude: " + userLong
+            elLon.innerHTML = "Your longtitude: " + userLon
             elduplze.innerHTML = "Distance to depulze, Cyberjaya is " + distances[0] + "km"
             elKlcc.innerHTML = "Distance to KLCC is " + distances[2] + "km"
             elSunway.innerHTML = "Distance to Sunway Pyramid is " + distances[1] + "km"
