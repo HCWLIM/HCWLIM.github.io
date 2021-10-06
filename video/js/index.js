de5dba8400b713fc7efbb101efefea08230662cd
Heng CW <weiwei5501@gmail.com>
2021-10-06 21:56:10 +0800

    2155

--- video/js/index.js
+++ video/js/index.js
@@ -1,32 +1,33 @@
 let video = document.getElementById("myVideo")
 
 let playBtn = document.getElementById("playBtn")
-playBtn.addEventListener("click", function(){
+playBtn.addEventListener("click",function(){
     video.play()
 })
 
 let pauseBtn = document.getElementById("pauseBtn")
-pauseBtn.addEventListener("click", function(){
+pauseBtn.addEventListener("click",function(){
     video.pause()
 })
 
 let freezeBtn = document.getElementById("freezeBtn")
-freezeBtn.addEventListener("mousedown", function(){
+freezeBtn.addEventListener("mousedown",function(){
     video.pause()
 })
-freezeBtn.addEventListener("mouseup", function(){
+freezeBtn.addEventListener("mouseup",function(){
     video.play()
-})
+})      
 
-let seekbar = document.getElementById("seekbar")
-seekbar.addEventListener("change", function(){
-    let time = video.duration * (seekbar.value/100)
+let seekBar = document.getElementById("seekBar")
+seekBar.addEventListener("change",function(){
+    let time = video.duration * (seekBar.value/100)
     video.currentTime = time
 })
 
-video.addEventListener("timeupdate", function(){
+video.addEventListener("timeupdate",function(){
     let value = (100/video.duration) * video.currentTime
-    seekbar.value = value
+    seekBar.value = value
+    console.log(video.currentTime)
 })
 
 let volumeControl = document.getElementById("volume")
